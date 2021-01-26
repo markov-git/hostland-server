3b59eee9ba8855efc14b250461926ab76a57eb09
markov-git <markovartyom795@gmail.com>
2021-01-26 15:09:59 +0300

    cors fix

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,8 @@
 module.exports = function(req, res, next) {
 
-  res.header("Access-Control-Allow-Origin", "*")
+  if (req.originalUrl === '/fetch') {
+    res.header("Access-Control-Allow-Origin", "*")
+  }
 
   next()
 }
